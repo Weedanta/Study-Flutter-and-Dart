6cea382a8ee31366d1c035bb2aae8f0fd20722c5
Weedanta <baguswedanta17@gmail.com>
2025-01-25 19:21:18 +0700

    Add: Widgets on Add and Update Page

--- lib/pages/add_and_update_todo.dart
+++ lib/pages/add_and_update_todo.dart
@@ -8,33 +8,68 @@ class AddAndUpdateTodo extends StatefulWidget {
 }
 
 class _AddAndUpdateTodoState extends State<AddAndUpdateTodo> {
+  TextEditingController title = TextEditingController();
+  TextEditingController description = TextEditingController();
+  bool isComplete = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-            backgroundColor: Colors.greenAccent,
-            title: const Text(
-              "Add Todo",
-              style: TextStyle(fontWeight: FontWeight.bold),
+        backgroundColor: Colors.greenAccent,
+        title: const Text(
+          "Add Todo",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            TextFormField(
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              decoration: InputDecoration(
+                hintText: 'Title',
+                border: InputBorder.none,
+              ),
             ),
-            centerTitle: true,
-          ),
-          body: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Title',
-                  border: InputBorder.none,
-                ),
+            TextFormField(
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+              decoration: InputDecoration(
+                hintText: 'Description',
+                border: InputBorder.none,
               ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Description',
-                  border: InputBorder.none,
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Complete',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
                 ),
-              ),
-            ],
-          ),
+                Switch(
+                  // This bool value toggles the switch.
+                  value: isComplete,
+                  activeColor: Colors.greenAccent,
+                  onChanged: (bool value) {
+                    // This is called when the user toggles the switch.
+                    setState(() {
+                      isComplete = value;
+                    });
+                  },
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Icon(Icons.done),
+      ),
     );
   }
 }
